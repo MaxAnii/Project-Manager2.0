3ec9688e495a6cfa91a06640818bb9ea260e9e9f
MaxAnii <ansarbaba2000@gmail.com>
2023-05-23 01:53:52 +0530

    added two signUp end points

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require ('dotenv').config();
 
 const express = require("express");
-const industryDetails = require('./routes/mainRoute')
+const checkRoute = require('./routes/mainRoute')
 const app = express();
 const cors = require('cors')
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use(industryDetails)
+app.use(checkRoute)
 
 
 
