9ed9eb0cd478ad9aa9f69868b46ce29473967078
MaxAnii <ansarbaba2000@gmail.com>
2024-01-14 19:57:29 +0530

    Change name to Project Harbor.

--- src/components/student/AddNewProject.jsx
+++ src/components/student/AddNewProject.jsx
@@ -1,230 +1,248 @@
-
-import { React,useEffect,useState } from 'react';
-import StudentNavbar from './StudentNavbar';
-import {  useParams } from 'react-router-dom'
-import Select from 'react-select';
-import { v4 } from 'uuid';
-import './student.css'
-import Footer from '../Footer';
+import { React, useEffect, useState } from "react";
+import StudentNavbar from "./StudentNavbar";
+import { useParams } from "react-router-dom";
+import Select from "react-select";
+import { v4 } from "uuid";
+import "./student.css";
+import Footer from "../Footer";
 const AddNewProject = () => {
   const param = useParams();
-  var memberOptions = []
-  var mentorOptions = []
+  var memberOptions = [];
+  var mentorOptions = [];
   let day = new Date().getDate();
-let month = new Date().getMonth();
-let year = new Date().getFullYear();
+  let month = new Date().getMonth();
+  let year = new Date().getFullYear();
 
-  const [ProjectInfo,setProjectInfo] = useState({
-    id:v4().slice(0,29),
-    projectName:"",
-    leaderId:param.id,
-    description:"",
-    mentorid:"",
-    status:'pending',
-   date: `${year}-${month+1}-${day}`
-    
-  })
-  const [projectType ,setProjectType] = useState("")
+  const [ProjectInfo, setProjectInfo] = useState({
+    id: v4().slice(0, 29),
+    projectName: "",
+    leaderId: param.id,
+    description: "",
+    mentorid: "",
+    status: "pending",
+    date: `${year}-${month + 1}-${day}`,
+  });
+  const [projectType, setProjectType] = useState("");
 
-  const [usn,setUsn] = useState([])
-    const [mentor,setMentor] = useState([])
-    const [projectMember, setProjectMember] =useState([])
+  const [usn, setUsn] = useState([]);
+  const [mentor, setMentor] = useState([]);
+  const [projectMember, setProjectMember] = useState([]);
 
-    const getData= async()=>{
-      if(projectType === 'interDepartment'){
-      const mentorResponse= await fetch(`http://localhost:5000/getProjectMentorList/${projectType}/${param.collegeCode}/${param.dname}`,
-      {
-        headers:{
-          JToken:localStorage.getItem("JToken")
-        }
-      }
-      )
-      const mentorData =await mentorResponse.json()
-      setMentor(mentorData)
-      const memberResponse = await fetch(`http://localhost:5000/getProjectMemberList/${projectType}/${param.collegeCode}/${param.dname}`,
-      {
-        headers:{
-          JToken:localStorage.getItem("JToken")
-        }
-      }
-      )
-      const memberData = await memberResponse.json()
-    setProjectMember(memberData)
-    }
-    else if(projectType === 'interCollege'){
-        const mentorResponse= await fetch(`http://localhost:5000/getProjectMentorList/${projectType}/${param.collegeCode}`,
+  const getData = async () => {
+    if (projectType === "interDepartment") {
+      const mentorResponse = await fetch(
+        `http://localhost:5000/getProjectMentorList/${projectType}/${param.collegeCode}/${param.dname}`,
         {
-          headers:{
-            JToken:localStorage.getItem("JToken")
-          }
+          headers: {
+            JToken: localStorage.getItem("JToken"),
+          },
         }
-        )
-        const mentorData =await mentorResponse.json()
-        setMentor(mentorData)
-      const memberResponse = await fetch(`http://localhost:5000/getProjectMemberList/${projectType}/${param.collegeCode}`,
-      {
-        headers:{
-          JToken:localStorage.getItem("JToken")
+      );
+      const mentorData = await mentorResponse.json();
+      setMentor(mentorData);
+      const memberResponse = await fetch(
+        `http://localhost:5000/getProjectMemberList/${projectType}/${param.collegeCode}/${param.dname}`,
+        {
+          headers: {
+            JToken: localStorage.getItem("JToken"),
+          },
         }
-      }
-      )
-      const memberData = await memberResponse.json()
-    setProjectMember(memberData)
+      );
+      const memberData = await memberResponse.json();
+      setProjectMember(memberData);
+    } else if (projectType === "interCollege") {
+      const mentorResponse = await fetch(
+        `http://localhost:5000/getProjectMentorList/${projectType}/${param.collegeCode}`,
+        {
+          headers: {
+            JToken: localStorage.getItem("JToken"),
+          },
+        }
+      );
+      const mentorData = await mentorResponse.json();
+      setMentor(mentorData);
+      const memberResponse = await fetch(
+        `http://localhost:5000/getProjectMemberList/${projectType}/${param.collegeCode}`,
+        {
+          headers: {
+            JToken: localStorage.getItem("JToken"),
+          },
+        }
+      );
+      const memberData = await memberResponse.json();
+      setProjectMember(memberData);
     }
-
-}
-  useEffect(()=>{
-    getData()
+  };
+  useEffect(() => {
+    getData();
     // eslint-disable-next-line
-  },[projectType])
+  }, [projectType]);
 
   //seting fetched usn as option list for select
-  for(var i=0;i<projectMember.length;i++){
-memberOptions[i] = {
-  value:projectMember[i].id,
-  label:projectMember[i].studentId,
-}
+  for (var i = 0; i < projectMember.length; i++) {
+    memberOptions[i] = {
+      value: projectMember[i].id,
+      label: projectMember[i].studentId,
+    };
   }
 
-  for(var i=0;i<mentor.length;i++){
-mentorOptions[i] = {
-  value:mentor[i].id,
-  label:mentor[i].name
-}
-}
-
-
-const submitProject=async(e)=>{
-  e.preventDefault();
-
- fetch("http://localhost:5000/addNewProject/project",{
-       method:"POST",
-       headers:{
-           JToken:localStorage.getItem('JToken'),
-           Accept:"application/json",
-           "Content-Type":"application/json"
-         },
-     body: JSON.stringify(ProjectInfo)
-   })
- 
-
-   for(let i=0;i<usn.length;i++){
-       const projectMemberDetails={
-          projectId:ProjectInfo.id,
-           memberId:usn[i].value,
-         }
-       fetch("http://localhost:5000/addNewProject/member",{
-             method:"POST",
-             headers:{
-                 JToken:localStorage.getItem('JToken'),
-                 Accept:"application/json",
-         "Content-Type":"application/json"
-       },
-       body: JSON.stringify(projectMemberDetails)
-     })
-   }
-
-   setProjectInfo({...ProjectInfo,projectName:"", 
-   description:"",
-   id:v4().slice(0,29)})
-   setUsn([])
-   alert("Project Submission Successful")
-
-
-}
-
-
-const handleChangeUsn=(selectedOption)=>{
-  var arr = selectedOption
-
-  setUsn(arr);
-  if(arr.length>4)
-  {
-    alert("Cannot Add more then 4 members")
-   setUsn(arr.slice(0,4))
+  for (var i = 0; i < mentor.length; i++) {
+    mentorOptions[i] = {
+      value: mentor[i].id,
+      label: mentor[i].name,
+    };
   }
-}
 
-const setMentorId=(selectedOption)=>{
- setProjectInfo({...ProjectInfo,mentorid:selectedOption.value})
-}
+  const submitProject = async (e) => {
+    e.preventDefault();
 
-    return (
+    fetch("http://localhost:5000/addNewProject/project", {
+      method: "POST",
+      headers: {
+        JToken: localStorage.getItem("JToken"),
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ProjectInfo),
+    });
+
+    for (let i = 0; i < usn.length; i++) {
+      const projectMemberDetails = {
+        projectId: ProjectInfo.id,
+        memberId: usn[i].value,
+      };
+      fetch("http://localhost:5000/addNewProject/member", {
+        method: "POST",
+        headers: {
+          JToken: localStorage.getItem("JToken"),
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(projectMemberDetails),
+      });
+    }
+
+    setProjectInfo({
+      ...ProjectInfo,
+      projectName: "",
+      description: "",
+      id: v4().slice(0, 29),
+    });
+    setUsn([]);
+    alert("Project Submission Successful");
+  };
+
+  const handleChangeUsn = (selectedOption) => {
+    var arr = selectedOption;
+
+    setUsn(arr);
+    if (arr.length > 4) {
+      alert("Cannot Add more then 4 members");
+      setUsn(arr.slice(0, 4));
+    }
+  };
+
+  const setMentorId = (selectedOption) => {
+    setProjectInfo({ ...ProjectInfo, mentorid: selectedOption.value });
+  };
+
+  return (
     <>
-<StudentNavbar id={param.id} dname={param.dname} collegeCode={param.collegeCode}></StudentNavbar>
+      <StudentNavbar
+        id={param.id}
+        dname={param.dname}
+        collegeCode={param.collegeCode}
+      ></StudentNavbar>
 
+      <div className="background">
+        <div className="container-content">
+          <h3 className="heading">Add new project with Project Harbor</h3>
+        </div>
+      </div>
 
-  <div className='background'>
+      <div className="project-form">
+        <form className="row g-3 FormContainer" onSubmit={submitProject}>
+          <div className="col-md-12 ">
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                id="inlineCheckbox1"
+                value="interDepartment"
+                onChange={(e) => setProjectType(e.target.value)}
+                name="projectType"
+              />
+              <label className="form-check-label">Inter Department</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                id="inlineCheckbox2"
+                value="interCollege"
+                onChange={() => setProjectType("interCollege")}
+                name="projectType"
+              />
+              <label className="form-check-label">Inter College</label>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">Project Name</label>
+            <input
+              type="text"
+              className="form-control"
+              value={ProjectInfo.projectName}
+              onChange={(e) => {
+                setProjectInfo({ ...ProjectInfo, projectName: e.target.value });
+              }}
+              required
+            />
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">Mentor</label>
+            <Select
+              options={mentorOptions}
+              onChange={setMentorId}
+              placeholder="Search Project Mentor"
+              noOptionsMessage={() => "No member found"}
+              required
+            />
+          </div>
+          <div className="col-12">
+            <label className="form-label">Project Description</label>
 
-<div className='container-content'>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              value={ProjectInfo.description}
+              onChange={(e) => {
+                setProjectInfo({ ...ProjectInfo, description: e.target.value });
+              }}
+              required
+            ></textarea>
+          </div>
+          <label className="form-label">Add Member</label>
 
-<h3 className='heading'>
-Add new project with Project Manager
-</h3>
+          <Select
+            options={memberOptions}
+            isMulti
+            onChange={handleChangeUsn}
+            value={usn}
+            placeholder="Search Project Member"
+            noOptionsMessage={() => "No member found"}
+          />
 
-</div>
-</div>
-
-<div className='project-form'>
-
-<form className="row g-3 FormContainer" onSubmit={submitProject}>
-<div className="col-md-12 ">
-<div className="form-check form-check-inline">
-  <input className="form-check-input" type="radio" id="inlineCheckbox1" value="interDepartment" onChange={e=>setProjectType(e.target.value)} name='projectType'/>
-  <label className="form-check-label" >Inter Department</label>
-</div>
-<div className="form-check form-check-inline">
-  <input className="form-check-input" type="radio" id="inlineCheckbox2" value="interCollege" onChange={()=>setProjectType("interCollege")}  name='projectType'/>
-  <label className="form-check-label" >Inter College</label>
-</div>
-  </div>
-  <div className="col-md-6">
-    <label  className="form-label">Project Name</label>
-    <input type="text" className="form-control" value={ProjectInfo.projectName}
-    onChange={e=>{setProjectInfo({...ProjectInfo,projectName:e.target.value})}}
-     required/>
-  </div>
-  <div className="col-md-6">
-    <label  className="form-label">Mentor</label>
-    <Select  
- options={mentorOptions} 
-  onChange={setMentorId}
-    placeholder="Search Project Mentor"
-    noOptionsMessage={()=> "No member found"}
-    required
-/>
-
-  </div>
-  <div className="col-12">
-    <label  className="form-label">Project Description</label>
-
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
-  value={ProjectInfo.description} onChange={e=>{setProjectInfo({...ProjectInfo,description:e.target.value})}}
-  required ></textarea>
-
-  </div>
-    <label  className="form-label">Add Member</label>
-
-<Select  
- options={memberOptions} isMulti
-   onChange={handleChangeUsn}
-    value={usn}
-    placeholder="Search Project Member"
-    noOptionsMessage={()=> "No member found"}
-/>
-
-
-  
-  <div className="col-12">
-  <button key="SubmitForm" type="submit" className="btn btn-success"  >Submit</button>
-  </div>
-  
-</form>
-
-</div>
-<Footer></Footer>
+          <div className="col-12">
+            <button key="SubmitForm" type="submit" className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <Footer></Footer>
     </>
-  )
-}
+  );
+};
 
 export default AddNewProject;
--- src/components/student/StudentNavbar.jsx
+++ src/components/student/StudentNavbar.jsx
@@ -1,78 +1,74 @@
 import React from "react";
-import { NavLink ,useNavigate} from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import checkAccess from "../../checkAcces";
 import Logout from "../Logout";
 
-
 const StudentNavbar = (props) => {
-  const navigate = useNavigate()
-  useEffect(()=>{
-    if(!checkAccess()) {
-      navigate('/')
-    } 
-  })
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!checkAccess()) {
+      navigate("/");
+    }
+  });
   return (
     <>
-      <div className="title"> 
-      <nav
-        className="navbar navbar-expand-lg"
-        
-      >
+      <div className="title">
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Project Harbor
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink
+                    to={`/StudentHome/${props.id}/${props.collegeCode}/${props.dname}`}
+                    className="navLink"
+                  >
+                    Home
+                  </NavLink>
+                </li>
 
-      
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#" >
-            Project Manager
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink
-                  to={`/StudentHome/${props.id}/${props.collegeCode}/${props.dname}`}
-                  className="navLink"
-                >
-                  Home
-                </NavLink>
-              </li>
+                <li className="nav-item">
+                  <NavLink
+                    to={`/AddNewProject/${props.id}/${props.collegeCode}/${props.dname}`}
+                    className="navLink"
+                  >
+                    New Project
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to={`/StudentHome/${props.id}/${props.collegeCode}/${props.dname}/personalInfo`}
+                    className="navLink"
+                  >
+                    My Account
+                  </NavLink>
+                </li>
 
-              <li className="nav-item">
-                <NavLink
-                  to={`/AddNewProject/${props.id}/${props.collegeCode}/${props.dname}`}
-                  className="navLink"
-                >
-                  New Project
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  to={`/StudentHome/${props.id}/${props.collegeCode}/${props.dname}/personalInfo`}
-                  
-                  className="navLink"
-                >
-                  My Account
-                </NavLink>
-              </li>
-
-              <li className="nav-item">
-                <Logout></Logout>
-            </li>
-            </ul>
+                <li className="nav-item">
+                  <Logout></Logout>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-        </div>
+        </nav>
+      </div>
     </>
   );
 };
