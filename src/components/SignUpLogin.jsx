56795044d372abbd983e3eaae95fcdb02e5502bd
MaxAnii <ansarbaba2000@gmail.com>
2024-02-19 00:05:14 +0530

    Added style to the form toggel buttons.

--- src/components/SignUpLogin.jsx
+++ src/components/SignUpLogin.jsx
@@ -1,41 +1,53 @@
-import React from 'react'
-import Title from './Title'
-import Footer from './Footer'
-import LoginForm from './LoginForm'
-import SignUp from './SignUp'
-import { useState } from 'react'
-import './signuplogin.css'
+import React from "react";
+import Title from "./Title";
+import Footer from "./Footer";
+import LoginForm from "./LoginForm";
+import SignUp from "./SignUp";
+import { useState } from "react";
+import "./signuplogin.css";
 const SignUpLogin = () => {
-  const [show,setShow]=useState(true)
-  return (
-    <div>
-    <Title></Title>
-    <div className="hero-section">
-      <h1 >Welcome to the Project Management System</h1>
-   </div>
-   <div className='form-container'>
+	const [show, setShow] = useState(true);
+	return (
+		<div>
+			<Title></Title>
+			<div className="hero-section">
+				<h1>Welcome to the Project Management System</h1>
+			</div>
+			<div className="form-container">
+				{show ? (
+					<>
+						<LoginForm></LoginForm>
+						<p>
+							Don't have an account?{" "}
+							<button
+								onClick={() => setShow(false)}
+								className="toggel btn btn-primary toggle-disabled"
+							>
+								SignUp
+							</button>
+						</p>
+					</>
+				) : (
+					<>
+						<SignUp></SignUp>
+						<p>
+							Already have an account?{" "}
+							<button
+								onClick={() => setShow(true)}
+								className="toggel btn btn-primary toggle-disabled"
+							>
+								login
+							</button>
+						</p>
+					</>
+				)}
+			</div>
 
+			<div className="form-footer">
+				<Footer></Footer>
+			</div>
+		</div>
+	);
+};
 
-
-
-
-       {show?<><LoginForm></LoginForm> 
-       <p>Don't have an account?  <button onClick={()=>setShow(false)} className='toggel'>SignUp</button></p>
-       </>:
-       <>
-
-       <SignUp></SignUp>
-       <p>Already have an account? <button onClick={()=>setShow(true)} className='toggel'>login</button></p>
-       </>
-       }
-      </div>
-
-      <div className='form-footer'>
-      <Footer></Footer>
-      </div>
-      
-    </div>
-  )
-}
-
-export default SignUpLogin
+export default SignUpLogin;
